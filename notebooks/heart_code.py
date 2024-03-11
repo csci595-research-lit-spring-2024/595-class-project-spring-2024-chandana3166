4330d3db8bc4764fd86eabc44dcb257f1d657917
Chandana_Narra <chandana.narra.007@gmail.com>
2024-03-10 23:28:29 -0500

    Methodology section data preparation techniques and code updated

--- notebooks/heart_code.py
+++ notebooks/heart_code.py
@@ -24,5 +24,33 @@ data_types = {
 # Convert the DataFrame columns to the specified data types
 df = df.astype(data_types)
 
-# Display the DataFrame
+# Display the DataFrame and datatypes
 print(df)
+print(df.dtypes)
+
+# Checking for missing values
+missing_values = df.isnull().sum()
+print("Missing Values:\n", missing_values)
+
+#Fill the mean of the values to handle missing values
+df_filled = df.fillna(df.mean())
+
+# drop rows with missing values
+# df_cleaned = df.dropna()
+
+# Get the number of rows before removing duplicates
+rows_before = df.shape[0]
+
+# Remove duplicates
+df.drop_duplicates(inplace=True)
+
+# Get the number of rows after removing duplicates
+rows_after = df.shape[0]
+
+# Calculate the number of duplicates removed
+duplicates_removed = rows_before - rows_after
+
+# Save the cleaned DataFrame back to a CSV file
+df.to_csv('cleaned_dataset.csv', index=False)
+
+print(f"Removed {duplicates_removed} duplicates.")
